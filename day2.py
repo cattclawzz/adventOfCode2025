90d9dc66484d7530a0c599741569c01cc64b1bdf
Anakin Edwards <anakinedwards9@gmail.com>
2025-12-02 14:51:24 +0000

    part 2 still broken... i honestly think i'm onto something here though

---
+++ day2.py
@@ -0,0 +1,24 @@
+input = '''11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'''
+input = [i.split('-') for i in input.split(',')]
+
+def part1(input):
+    password = 0
+    for rng in input:
+        for i in range(int(rng[0]),int(rng[1])+1):
+            j = str(i)
+            k = len(j)//2
+            if j[k:] == j[:k]:
+                password += i
+    return password
+
+def part2(input):
+    password = 0
+    for rng in input:
+        for i in range(int(rng[0]),int(rng[1])+1):
+            j = str(i)
+            k = len(j)//2
+            if any([''.join(j.split(j[:x])) == '' for x in range(1,k)]):
+                password += i
+    return password
+
+print(f"Part 1: {part1(input)} \nPart 2: {part2(input)}")
